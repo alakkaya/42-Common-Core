0be448a142a63b3c3e100ce07f92d5649859c523
aliAkkayaJ <aliakkayaa0@gmail.com>
2024-10-14 20:52:52 +0300

    calloc added

--- 1.libft/libft.h
+++ 1.libft/libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aliakkay <aliakkay@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ali <ali@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 12:52:45 by aliakkay          #+#    #+#             */
-/*   Updated: 2024/10/13 21:19:03 by aliakkay         ###   ########.fr       */
+/*   Updated: 2024/10/14 20:48:31 by ali              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #define LIBFT_H
 
 #include <stddef.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
@@ -23,7 +24,7 @@ int ft_toupper(int c);
 int ft_tolower(int c);
 int ft_strncmp(const char *s1, const char *s2, size_t n);
 int ft_memcmp(const void *s1, const void *s2, size_t n);
-int	ft_atoi(const char *str);
+int ft_atoi(const char *str);
 
 char *ft_strchr(const char *s, int c);
 char *ft_strrchr(const char *s, int c);
@@ -38,5 +39,6 @@ void *ft_bzero(void *s, size_t n);
 void *ft_memcpy(void *dst, const void *src, size_t n);
 void *ft_memmove(void *dst, const void *src, size_t len);
 void *ft_memchr(const void *s, int c, size_t n);
+void *ft_calloc(size_t count, size_t size);
 
 #endif
