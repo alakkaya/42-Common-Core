79a785094604fdd743594bc19d183067f2169afe
alakkaya <aliakkayaa0@gmail.com>
2024-10-24 19:39:22 +0300

    ft_putstr_fd added

---
+++ 1.libft/ft_putstr_fd.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aliakkay <aliakkay@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/24 19:11:07 by aliakkay          #+#    #+#             */
+/*   Updated: 2024/10/24 19:38:09 by aliakkay         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <fcntl.h>
+
+void    ft_putstr_fd(char *s, int fd)
+{
+    size_t  i;
+
+    i = 0;
+    while (s[i])
+    {
+        write(fd,&s[i],1);
+        i++;
+    }
+}
+int main()
+{
+    int fd = open("alı.text", O_WRONLY | O_CREAT | O_APPEND, 0644);
+    ft_putstr_fd("aliakkaya",fd);
+}
