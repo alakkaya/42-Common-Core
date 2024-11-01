62ccbaec08f083634d374febfa1dbc558b69989d
alakkaya <aliakkayaa0@gmail.com>
2024-11-01 18:59:47 +0300

    strchr and strlcat changed

--- 1.libft/ft_strlcat.c
+++ 1.libft/ft_strlcat.c
@@ -3,35 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ali <ali@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: aliakkay <aliakkay@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 15:46:27 by aliakkay          #+#    #+#             */
-/*   Updated: 2024/10/27 19:49:56 by ali              ###   ########.fr       */
+/*   Updated: 2024/11/01 18:59:34 by aliakkay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+size_t	ft_strlcat(char	*dst, const char *src, size_t dstsize)
 {
 	size_t	i;
-	size_t	j;
+	size_t	a;
+	size_t	sizedst;
+	size_t	sizesrc;
 
-	j = 0;
+	sizedst = ft_strlen(dst);
+	sizesrc = ft_strlen(src);
+	a = ft_strlen(dst);
+	if (dstsize <= sizedst)
+		return (dstsize + sizesrc);
 	i = 0;
-	while (dst[i] && i < dstsize)
-		i++;
-	if (dstsize > 0 && i < dstsize - 1)
+	while (src[i] != '\0' && a < dstsize - 1)
 	{
-		while (src[j] && i < dstsize - 1)
-		{
-			dst[i] = src[j];
-			i++;
-			j++;
-		}
-		dst[i] = '\0';
+		dst[a] = src[i];
+		a++;
+		i++;
 	}
-	while (src[j])
-		j++;
-	return (i + j);
+	dst[a] = '\0';
+	return (sizedst + sizesrc);
 }
